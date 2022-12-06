0470c6f303dce3941d859a0f1994a10af5b15417
Loosen-IT <janniclas@loosen-it.de>
2022-12-06 12:15:50 +0100

    uh

--- uninstall.php
+++ uninstall.php
@@ -1,5 +1,14 @@
 <?php
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) { exit(); }
 require_once(plugin_dir_path(__FILE__).'database/data_control.php');
-delete_all_databases();
+global $wpdb;
+$main = $wpdb->prefix.'super_main';
+$colors = $wpdb->prefix.'super_colors';
+$capabilities = $wpdb->prefix.'super_capabilities';
+$menus = $wpdb->prefix.'super_menus';
+
+$wpdb-> query ("DROP TABLE IF EXISTS {$main}");
+$wpdb-> query ("DROP TABLE IF EXISTS {$colors}");
+$wpdb-> query ("DROP TABLE IF EXISTS {$capabilities}");
+$wpdb-> query ("DROP TABLE IF EXISTS {$menus}");
 ?>
