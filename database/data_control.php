06249008c1a922d814648a01ac0621a7d416b56f
Loosen-IT <janniclas@loosen-it.de>
2022-12-08 23:07:04 +0100

    dfgf

--- database/data_control.php
+++ database/data_control.php
@@ -81,35 +81,32 @@ function delete_all_databases(){
 }
 
 //Checks the current plugin-version and update/create tables if needed
-function check_requirements($version){
+function check_requirements($version)
+{
     global $wpdb;
-    require_once(ABSPATH.'wp-admin/includes/upgrade.php');
-    $main_scheme="CREATE TABLE ".add_prefix('super_main')."(version VARCHAR(20),dummy_bool boolean DEFAULT true,debug_mode boolean DEFAULT false,quick_copy_pages boolean DEFAULT false,quick_copy_posts boolean DEFAULT false,custom_colors boolean DEFAULT false,custom_menus boolean DEFAULT false, PRIMARY KEY(version))";
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    $main_scheme = "CREATE TABLE " . add_prefix('super_main') . "(version VARCHAR(20),dummy_bool boolean DEFAULT true,debug_mode boolean DEFAULT false,quick_copy_pages boolean DEFAULT false,quick_copy_posts boolean DEFAULT false,custom_colors boolean DEFAULT false,custom_menus boolean DEFAULT false, PRIMARY KEY(version))";
     //Checks existence of main-table (lists functions of super-plugin) and creates if not
-    if($wpdb-> get_var("SHOW TABLES LIKE ".add_prefix('super_main')) != add_prefix('super_main')){
+    if ($wpdb->get_var("SHOW TABLES LIKE " . add_prefix('super_main')) != add_prefix('super_main')) {
         dbDelta($main_scheme);
-        insert_into_database('super_main','version',$version);
+        insert_into_database('super_main', 'version', $version);
     }
     //Checks the current version of main-table and updates if not
-    if (get_database_value('super_menu','version') != $version) {
-        $values = ($wpdb->get_results("SELECT * FROM ".add_prefix('super_main'), ARRAY_A))[0];
+    if (get_database_value('super_menu', 'version') != $version) {
+        $values = ($wpdb->get_results("SELECT * FROM " . add_prefix('super_main'), ARRAY_A))[0];
         $wpdb->flush();
-        $wpdb->query("DROP TABLE ".add_prefix('super_main'));
+        $wpdb->query("DROP TABLE " . add_prefix('super_main'));
         dbDelta($main_scheme);
-        $values['version']=$version;
+        $values['version'] = $version;
         $wpdb->insert(add_prefix('super_main'), $values);
     }
     //Checks existence of color-table (for color-customizer) and creates if not
-    if($wpdb-> get_var("SHOW TABLES LIKE ".add_prefix('super_colors')) != add_prefix('super_colors')){
-        dbDelta("CREATE TABLE ".add_prefix('super_colors')."(dummy_bool boolean DEFAULT true,color_base VARCHAR(7) DEFAULT '#3e94c3',color_text VARCHAR(7) DEFAULT '#ffffff',color_high VARCHAR(7) DEFAULT '#d54e21',color_subh VARCHAR(7) DEFAULT '#0067bc',color_note VARCHAR(7) DEFAULT '#d54e21',color_link VARCHAR(7) DEFAULT '#1e73be',color_form VARCHAR(7) DEFAULT '#3e94c3',color_back VARCHAR(7) DEFAULT '#ffffff',PRIMARY KEY(dummy_bool))");
-        insert_into_database('super_colors','dummy_bool',true);
+    if ($wpdb->get_var("SHOW TABLES LIKE " . add_prefix('super_colors')) != add_prefix('super_colors')) {
+        dbDelta("CREATE TABLE " . add_prefix('super_colors') . "(dummy_bool boolean DEFAULT true,color_base VARCHAR(7) DEFAULT '#3e94c3',color_text VARCHAR(7) DEFAULT '#ffffff',color_high VARCHAR(7) DEFAULT '#d54e21',color_subh VARCHAR(7) DEFAULT '#0067bc',color_note VARCHAR(7) DEFAULT '#d54e21',color_link VARCHAR(7) DEFAULT '#1e73be',color_form VARCHAR(7) DEFAULT '#3e94c3',color_back VARCHAR(7) DEFAULT '#ffffff',PRIMARY KEY(dummy_bool))");
+        insert_into_database('super_colors', 'dummy_bool', true);
     }
     //Checks existence of menu-table (old and new names) and creates if not
-    if($wpdb-> get_var("SHOW TABLES LIKE ".add_prefix('super_menus')) != add_prefix('super_menus')){
-        dbDelta("CREATE TABLE ".add_prefix('super_menus')."(slug VARCHAR(128),old_name VARCHAR(128),new_name VARCHAR(128),PRIMARY KEY(old_name,slug))");
-    }
-    //Checks existence of capability-table (original menu and new capability) and creates if not
-    if($wpdb-> get_var("SHOW TABLES LIKE ".add_prefix('super_capabilities')) != add_prefix('super_capabilities')){
-        dbDelta("CREATE TABLE ".add_prefix('super_capabilities')."(slug VARCHAR(128),org_name VARCHAR(128),capability VARCHAR(128),PRIMARY KEY(org_name))");
+    if ($wpdb->get_var("SHOW TABLES LIKE " . add_prefix('super_menus')) != add_prefix('super_menus')) {
+        dbDelta("CREATE TABLE " . add_prefix('super_menus') . "(slug VARCHAR(128),old_name VARCHAR(128),new_name VARCHAR(128),capability VARCHAR(128),PRIMARY KEY(old_name,slug))");
     }
 }
